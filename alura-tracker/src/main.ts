08025684bafeb715e471287c69243d96c4ca9b8a
alnbastos <alinebastos23@outlook.com>
2024-06-24 21:24:38 -0300

    feat: um estado para a todos governar (store)

--- alura-tracker/src/main.ts
+++ alura-tracker/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import roteador from "./router";
+import { key, store } from "./store";
 
 import "@fortawesome/fontawesome-free/css/all.css";
 
 createApp(App)
     .use(roteador)
+    .use(store, key)
     .mount("#app")
